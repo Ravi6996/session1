04fc7aadc5657c9abace0aeea4c4f3c6343425e9
ravi_kumar <ravikumardubey215@gmail.com>
2022-06-09 19:21:27 +0530

    branch added

---
+++ hello2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("hello world\n");
+    printf("ravi kumar\n");
+    printf("my id is b421039\n");
+    printf("my branch is IT\n");
+
+    return 0;
+}
